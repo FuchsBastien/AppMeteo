81c97b4ff1af9dce2f9195f5b3a4016a06ed3617
Bastien FUCHS <https://github.com/FuchsBastien/FuchsBastien_5_07092021.git>
2022-01-10 17:32:57 +0100

    afficher données bloc info

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 const CLEAPI = 'dc6213a6c3ef2d2335152ed981eadef4';
 let resultatsAPI ;
 
-/*fonction si localisation activé*/
+const temps = document.querySelector('.temps');
+const temperature = document.querySelector('.temperature');
+const localisation = document.querySelector('.localisation');
+
+/*condition si localisation activée ou désactivée*/
 if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
 
-        console.log(position);
+        //console.log(position);
         let long = position.coords.longitude;
         let lat = position.coords.latitude;
         AppelAPI(long,lat);
@@ -15,15 +19,27 @@ if(navigator.geolocation) {
     })
 }
 
+
+ /*fonction appel API*/
 function AppelAPI(long, lat) {
       //console.log(long, lat)
       //on ajoute ${} et &exclude=minutely&units=metric&lang=fr pour exclure min, unité en m et en français
       fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${CLEAPI}`)
+      //on transforme les données en fichier json
       .then((reponse) => {
           return reponse.json();
       })
+      //on stocke les données du fichier json dans une variable
       .then((data) => {
           console.log(data)
+          resultatsAPI = data;
+            
+        //on appelle des données de ce fichier json
+        temps.innerText = resultatsAPI.current.weather[0].description;
+        temperature.innerText = `${Math.trunc(resultatsAPI.current.temp)}°`
+        localisation.innerText = resultatsAPI.timezone;
       }) 
+
+     
    
 }         
