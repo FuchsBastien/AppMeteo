4316789b96b47d2b82ff44485b019eead433f53d
Bastien FUCHS <https://github.com/FuchsBastien/FuchsBastien_5_07092021.git>
2022-01-10 17:47:07 +0100

    afficher données bloc h heure

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,8 @@ let resultatsAPI ;
 const temps = document.querySelector('.temps');
 const temperature = document.querySelector('.temperature');
 const localisation = document.querySelector('.localisation');
+const heure = document.querySelectorAll('.heure-nom-prevision');
+const tempPourH = document.querySelectorAll('.heure-prevision-valeur');
 
 /*condition si localisation activée ou désactivée*/
 if(navigator.geolocation) {
@@ -20,7 +22,7 @@ if(navigator.geolocation) {
 }
 
 
- /*fonction appel API*/
+/*fonction appel API*/
 function AppelAPI(long, lat) {
       //console.log(long, lat)
       //on ajoute ${} et &exclude=minutely&units=metric&lang=fr pour exclure min, unité en m et en français
@@ -36,10 +38,41 @@ function AppelAPI(long, lat) {
             
         //on appelle des données de ce fichier json
         temps.innerText = resultatsAPI.current.weather[0].description;
+        //on utilise les `` pour rajouter le signe des degrés °, ${} pour inclure les données et math.trunc pour enlever les chiffres après la virgule
+        
         temperature.innerText = `${Math.trunc(resultatsAPI.current.temp)}°`
         localisation.innerText = resultatsAPI.timezone;
       }) 
 
-     
+
+      // les heures, par tranche de trois, avec leur temperature.
+      //heure actuel
+      let heureActuelle = new Date().getHours();
+
+      for(let i = 0; i < heure.length; i++) {
+          
+         // on ajoute 3h à chaque fois
+          let heureIncr = heureActuelle + i * 3;
+
+          heure[i].innerText = `${heureIncr} h`;
+
+         
+
+      }
    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }         
